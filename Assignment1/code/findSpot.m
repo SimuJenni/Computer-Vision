2f96d8b378b8319d4d4008a9abb0c4de39b69b23
Simon Jenni <simujenni@students.unibe.ch>
2015-10-08 11:31:52 +0200

    Initial commit and first parts of the first assignement

---
+++ Assignment1/code/findSpot.m
@@ -0,0 +1,12 @@
+function [ spotPosition ] = findSpot( I )
+%Finds the position of brightest spot in an image. 
+%   Input is an m*n image and the function outputs a vector indicating the
+%   postition of the brightest spot in the image
+
+m = max(I(:));
+idx = find(I(:)>0.95*m);    % find brightest pixels
+
+[row, col] = ind2sub(size(I), idx);
+spotPosition = mean([row, col]);
+end
+
